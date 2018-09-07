3567f3458b99554fe98702947b76ac5bdee145de
Ross Cooper <rossjamescooper@gmail.com>
2018-09-07 16:06:35 -0700

    Tried my first Typescript React component

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
+// import App from './App';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
+import Hello from './Hello';
 
 ReactDOM.render(
-  <App />,
+  <Hello name="Ross" enthusiasmLevel={10} />,
+  // <App />,
   document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
