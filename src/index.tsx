c9d1a887bb7d42c1f4db71372f8f66e575b659ba
Ross Cooper <rossjamescooper@gmail.com>
2018-09-08 15:41:31 -0700

    Added stateful component, working towards creating my own Typescript component soon

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-// import App from './App';
-import './index.css';
+// import App from './components/App';
+import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
-import Hello from './Hello';
+// import Hello from './components/Hello';
+import StatefulHello from './components/StatefulHello'
 
 ReactDOM.render(
-  <Hello name="Ross" enthusiasmLevel={10} />,
+  <StatefulHello name="Ross" enthusiasmLevel={10} />,
   // <App />,
   document.getElementById('root') as HTMLElement
 );
