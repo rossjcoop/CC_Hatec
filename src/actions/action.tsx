63cea745aadf46fa5fd2a4c7e4e30f7c6fe4029f
Ross Cooper <rossjamescooper@gmail.com>
2018-09-10 15:33:00 -0700

    Going to shelve ts for awhile, need to take more time to learn

--- src/actions/action.tsx
+++ src/actions/action.tsx
@@ -1,9 +1,10 @@
 
-import axios from "axios"
-import * as actionValues from './actionValues'
+import store from "../store";
+import axios, { AxiosRequestConfig, AxiosPromise } from "axios";
+import * as actionValues from './actionValues';
 
 
-const url = "https://jsonplaceholder.typicode.com/"
+const api = "https://jsonplaceholder.typicode.com/"
 
 
 export interface GetAll {
@@ -17,11 +18,16 @@ export interface GetPostsAndComments {
 export type DataRetrieval = GetAll | GetPostsAndComments;
 
 
-export function getAllData(): GetAll {
-    
-    
-    
-    return {
+export function getAllData(): GetAll { 
+    return axios.all([
+        axios.get(api + 'posts');
+        axios.get(api + 'users')
+      ])
+      .then(axios.spread(function (userResponse, reposResponse) {
+        //... but this callback will be executed only when both requests are complete.
+        console.log('User', userResponse.data);
+        console.log('Repositories', reposResponse.data);
+      }));
         type: actionValues.GET_ALL
     }
 }
@@ -32,3 +38,12 @@ export function decrementEnthusiasm(): DecrementEnthusiasm {
     }
 }
 
+axios.all([
+    axios.get('https://api.github.com/users/codeheaven-io');
+    axios.get('https://api.github.com/users/codeheaven-io/repos')
+  ])
+  .then(axios.spread(function (userResponse, reposResponse) {
+    //... but this callback will be executed only when both requests are complete.
+    console.log('User', userResponse.data);
+    console.log('Repositories', reposResponse.data);
+  }));
